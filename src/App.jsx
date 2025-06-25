03a8f13543dccd9a18dee862506b0d6ec4989596
0xDev05 <divpatel1274@gmail.com>
2025-06-25 15:56:31 +0530

    functionality for creating token added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import './App.css'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import "@solana/wallet-adapter-react-ui/styles.css";
+
 
 function App() {
-  return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    return (
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]}>
+          <WalletModalProvider>
+            <WalletMultiButton />
+            <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    )
 }
 
 export default App
--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,5 +1,43 @@
 
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+
+
 export function TokenLaunchpad() {
+    const wallet = useWallet();
+    const {connection} = useConnection();
+
+    async function creteToken(){
+        console.log("creating a token");
+        if(!wallet.publicKey || !connection){
+            alert("connect wallet first");
+        }
+        
+
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const keyPair = Keypair.generate();
+        const programId = TOKEN_PROGRAM_ID;
+        const decimals = 9;
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keyPair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId,
+            }),
+            createInitializeMint2Instruction(keyPair.publicKey, decimals, wallet.publicKey, wallet.publicKey, programId),
+        );
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.partialSign(keyPair);
+
+        const sign = await wallet.sendTransaction(transaction,connection);
+        console.log("token created succesfully...",sign);
+
+    }
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -8,10 +46,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <input className='inputText' type='text' placeholder='Name' id="name"></input> <br />
+        <input className='inputText' type='text' placeholder='Symbol' id="symbol"></input> <br />
+        <input className='inputText' type='text' placeholder='Image URL' id="imageUrl"></input> <br />
+        <input className='inputText' type='text' placeholder='Initial Supply' id="supply"></input> <br />
+        <button className='btn' onClick={creteToken}>Create a token</button>
     </div>
 }
