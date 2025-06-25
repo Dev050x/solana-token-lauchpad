24491478229e74bc25d324ca84a1d82d0299cdc0
0xDev05 <divpatel1274@gmail.com>
2025-06-25 16:36:00 +0530

    ast and minting

--- src/components/TokenLaunchpad.jsx
+++ src/components/TokenLaunchpad.jsx
@@ -1,7 +1,7 @@
 
-import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint , createAssociatedTokenAccountInstruction, getAssociatedTokenAddressSync, createMintToInstruction } from "@solana/spl-token";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, SystemProgram, Transaction } from "@solana/web3.js";
 
 
 export function TokenLaunchpad() {
@@ -35,6 +35,25 @@ export function TokenLaunchpad() {
 
         const sign = await wallet.sendTransaction(transaction,connection);
         console.log("token created succesfully...",sign);
+        
+        const asscociatedToken = getAssociatedTokenAddressSync(keyPair.publicKey,wallet.publicKey,TOKEN_PROGRAM_ID);
+        const transaction2 = new Transaction().add(
+            createAssociatedTokenAccountInstruction(
+                wallet.publicKey,
+                asscociatedToken,
+                wallet.publicKey,
+                keyPair.publicKey,
+                TOKEN_PROGRAM_ID
+            )
+        )
+        const sig2 = await wallet.sendTransaction(transaction2,connection);
+        console.log("creating accont for the token mint (ast): ,",sig2);
+
+        const transaction3 = new Transaction().add(
+            createMintToInstruction(keyPair.publicKey,asscociatedToken,wallet.publicKey,1*LAMPORTS_PER_SOL,[],TOKEN_PROGRAM_ID),
+        )
+        const sig3 = await wallet.sendTransaction(transaction3,connection);
+        console.log("minting token to your account",sig3);
 
     }
 
